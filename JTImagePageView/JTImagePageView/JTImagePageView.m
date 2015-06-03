049b81a7d1bf73aa8a761266e6b0b708b202017f
caijingzhuang <caijingzhuang@dongzone.com>
2015-06-03 22:31:53 +0800

    first commit

---
+++ JTImagePageView/JTImagePageView/JTImagePageView.m
@@ -0,0 +1,181 @@
+//
+//  JTImagePageView.m
+//  JTImagePageView
+//
+//  Created by John Tsai on 15/6/3.
+//  Copyright (c) 2015年 John Tsai. All rights reserved.
+//
+
+#import "JTImagePageView.h"
+#import "JTImageViewCell.h"
+
+static NSString *const identifier = @"JTImageViewCellID";
+
+@interface JTImagePageView ()
+<UICollectionViewDataSource,UICollectionViewDelegate>
+
+@property (nonatomic, strong) UICollectionView *collectionView;
+
+@end
+
+@implementation JTImagePageView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setUp];
+    }
+    return self;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        [self setUp];
+    }
+    return self;
+}
+
+- (void)setUp {
+    
+    // set collectionview flow layout
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    layout.minimumInteritemSpacing = 0;
+    layout.minimumLineSpacing = 0;
+    layout.itemSize = self.frame.size;
+    
+    // init collectionview
+    self.collectionView = [[UICollectionView alloc] initWithFrame:self.frame collectionViewLayout:layout];
+    self.collectionView.dataSource = self;
+    self.collectionView.pagingEnabled = YES;
+    self.collectionView.delegate = self;
+    self.collectionView.showsHorizontalScrollIndicator = NO;
+    self.collectionView.contentSize = CGSizeMake(MAXFLOAT, CGRectGetHeight(self.frame));
+    [self addSubview:self.collectionView];
+    
+    [self.collectionView registerNib:[UINib nibWithNibName:@"JTImageViewCell" bundle:nil] forCellWithReuseIdentifier:identifier];
+}
+
+- (CGSize)intrinsicContentSize  {
+    return CGSizeMake(375, 200);
+}
+
+#pragma mark -
+#pragma mark UICollectionViewDataSource
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.images.count ;
+}
+
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    JTImageViewCell *cell = (JTImageViewCell *)[collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
+    
+    cell.imageView.image = [self.images objectAtIndex:indexPath.row % self.images.count];
+    
+    return cell;
+}
+
+#pragma mark -
+#pragma mark UICollectionViewDelegate
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+        [self.delegate imagePageView:self didSelectIndex:indexPath.row % self.images.count];
+}
+
+/**
+ *  this will be another way to implementation infinite scroll
+ */
+//- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+//    float contentOffsetWhenFullyScrolledRight = self.collectionView.frame.size.width * ([self.images count] -1);
+//    float offsetX = scrollView.contentOffset.x;
+//    NSLog(@"%f",offsetX);
+//    if (scrollView.contentOffset.x == contentOffsetWhenFullyScrolledRight) {
+//        
+//        // user is scrolling to the right from the last item to the 'fake' item 1.
+//        // reposition offset to show the 'real' item 1 at the left-hand end of the collection view
+//        
+//        NSIndexPath *newIndexPath = [NSIndexPath indexPathForItem:1 inSection:0];
+//        
+//        [self.collectionView scrollToItemAtIndexPath:newIndexPath atScrollPosition:UICollectionViewScrollPositionLeft animated:NO];
+//        
+//    } else if (scrollView.contentOffset.x == 0)  {
+//        
+//        // user is scrolling to the left from the first item to the fake 'item N'.
+//        // reposition offset to show the 'real' item N at the right end end of the collection view
+//        
+//        NSIndexPath *newIndexPath = [NSIndexPath indexPathForItem:([self.images count] -2) inSection:0];
+//        
+//        [self.collectionView scrollToItemAtIndexPath:newIndexPath atScrollPosition:UICollectionViewScrollPositionLeft animated:NO];
+//        
+//    }
+//}
+
+- (void) scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (self.images.count <= 1)
+        return;
+    
+    static CGFloat lastContentOffsetX = FLT_MIN;
+    
+    // We can ignore the first time scroll,
+    // because it is caused by the call scrollToItemAtIndexPath: in ViewWillAppear
+    if (FLT_MIN == lastContentOffsetX) {
+        lastContentOffsetX = scrollView.contentOffset.x;
+        return;
+    }
+    
+    CGFloat currentOffsetX = scrollView.contentOffset.x;
+    CGFloat currentOffsetY = scrollView.contentOffset.y;
+    
+    CGFloat pageWidth = scrollView.frame.size.width;
+    CGFloat offset = pageWidth * (self.images.count - 2);
+    
+    // the first page(showing the last item) is visible and user is still scrolling to the left
+    if (currentOffsetX < pageWidth && lastContentOffsetX > currentOffsetX) {
+        lastContentOffsetX = currentOffsetX + offset;
+        scrollView.contentOffset = (CGPoint){lastContentOffsetX, currentOffsetY};
+    }
+    // the last page (showing the first item) is visible and the user is still scrolling to the right
+    else if (currentOffsetX > offset && lastContentOffsetX < currentOffsetX) {
+        lastContentOffsetX = currentOffsetX - offset;
+        scrollView.contentOffset = (CGPoint){lastContentOffsetX, currentOffsetY};
+    } else {
+        lastContentOffsetX = currentOffsetX;
+    }
+}
+
+#pragma mark -
+#pragma mark ReloadData
+- (void)reloadData {
+    if (self.images.count > 1)
+    [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForItem:1 inSection:0]atScrollPosition:UICollectionViewScrollPositionLeft animated:NO];
+    [self.collectionView reloadData];
+}
+
+#pragma mark -
+#pragma mark SetDataArray
+- (void)setImages:(NSArray *)images {
+    if (images.count <= 1) {
+        _images = images;
+        return;
+    }
+    
+    NSMutableArray *array = [NSMutableArray arrayWithArray:images];
+    [array addObject:[images firstObject]];
+    [array insertObject:[images lastObject] atIndex:0];
+    _images = [NSArray arrayWithArray:array];
+    
+    [self reloadData];
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
--- JTImagePageView/ViewController.m
+++ JTImagePageView/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "JTImagePageView.h"
 
 @interface ViewController ()
+<JTImagePageViewDelegate>
 
 @end
 
@@ -17,11 +19,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    ;
+    JTImagePageView *imagePageView = [[JTImagePageView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 200)];
+    
+    @autoreleasepool {
+        
+        UIImage *image1 = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"cat0" ofType:@"jpg"]];
+        UIImage *image2 = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"cat1" ofType:@"jpg"]];
+        UIImage *image3 = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"cat2" ofType:@"jpg"]];
+        UIImage *image4 = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"cat4" ofType:@"jpg"]];
+        UIImage *image5 = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"cat5" ofType:@"jpg"]];
+        UIImage *image6 = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"cat6" ofType:@"jpg"]];
+        UIImage *image7 = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"cat7" ofType:@"jpg"]];
+        UIImage *image8 = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"dog0" ofType:@"jpg"]];
+        UIImage *image9 = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"dog1" ofType:@"jpg"]];
+        
+        NSArray *images = [NSArray arrayWithObjects:image1,image2,image3,image4,image5,image6,image7,image8,image9, nil];
+        imagePageView.images = images;
+        imagePageView.delegate = self;
+    }
+    [self.view addSubview:imagePageView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)imagePageView:(JTImagePageView *)view didSelectIndex:(NSUInteger)index {
+    NSLog(@"index is %lu",index);
 }
 
 @end
