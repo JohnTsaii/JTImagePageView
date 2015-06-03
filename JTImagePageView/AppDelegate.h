6be789419afc9fda704d2cb52cf9fd6f0e6880d6
caijingzhuang <caijingzhuang@dongzone.com>
2015-06-03 11:18:53 +0800

    Initial Commit

---
+++ JTImagePageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JTImagePageView
+//
+//  Created by CC on 15/6/3.
+//  Copyright (c) 2015年 John Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JTImagePageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JTImagePageView
+//
+//  Created by CC on 15/6/3.
+//  Copyright (c) 2015年 John Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
