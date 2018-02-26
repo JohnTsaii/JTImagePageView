96848cd7e48d2cdbfd21e934f4786df6af528047
caijingzhuang <caijingzhuang@gmail.com>
2018-02-26 10:20:17 +0800

    create framewrok target for carthage

---
+++ JTImagePageViewFrameWork/JTImagePageViewFrameWork.h
@@ -0,0 +1,20 @@
+//
+//  JTImagePageViewFrameWork.h
+//  JTImagePageViewFrameWork
+//
+//  Created by John TSai on 2018/2/26.
+//  Copyright © 2018年 John Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JTImagePageViewFrameWork.
+FOUNDATION_EXPORT double JTImagePageViewFrameWorkVersionNumber;
+
+//! Project version string for JTImagePageViewFrameWork.
+FOUNDATION_EXPORT const unsigned char JTImagePageViewFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTImagePageViewFrameWork/PublicHeader.h>
+
+
+#import "JTImagePageView.h"
